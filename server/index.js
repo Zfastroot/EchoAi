129cfdeee86a61652aec2b5e9ece2891b1378d10
hajarloraoui <hajar.loraoui12@gmail.com>
2024-08-27 00:21:34 +0000

    finish the upload & the prompt

--- server/index.js
+++ server/index.js
@@ -42,7 +42,125 @@ app.get("/api/upload", (req, res) => {
   const result = imagekit.getAuthenticationParameters();
   res.send(result);
 });
-
+app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+    const { text } = req.body;
+  
+    try {
+      // CREATE A NEW CHAT
+      const newChat = new Chat({
+        userId: userId,
+        history: [{ role: "user", parts: [{ text }] }],
+      });
+  
+      const savedChat = await newChat.save();
+  
+      // CHECK IF THE USERCHATS EXISTS
+      const userChats = await UserChats.find({ userId: userId });
+  
+      // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+      if (!userChats.length) {
+        const newUserChats = new UserChats({
+          userId: userId,
+          chats: [
+            {
+              _id: savedChat._id,
+              title: text.substring(0, 40),
+            },
+          ],
+        });
+  
+        await newUserChats.save();
+      } else {
+        // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+        await UserChats.updateOne(
+          { userId: userId },
+          {
+            $push: {
+              chats: {
+                _id: savedChat._id,
+                title: text.substring(0, 40),
+              },
+            },
+          }
+        );
+  
+        res.status(201).send(newChat._id);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error creating chat!");
+    }
+  });
+  
+  app.get("/api/userchats", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+  
+    try {
+      const userChats = await UserChats.find({ userId });
+  
+      res.status(200).send(userChats[0].chats);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error fetching userchats!");
+    }
+  });
+  
+  app.get("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+  
+    try {
+      const chat = await Chat.findOne({ _id: req.params.id, userId });
+  
+      res.status(200).send(chat);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error fetching chat!");
+    }
+  });
+  
+  app.put("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+  
+    const { question, answer, img } = req.body;
+  
+    const newItems = [
+      ...(question
+        ? [{ role: "user", parts: [{ text: question }], ...(img && { img }) }]
+        : []),
+      { role: "model", parts: [{ text: answer }] },
+    ];
+  
+    try {
+      const updatedChat = await Chat.updateOne(
+        { _id: req.params.id, userId },
+        {
+          $push: {
+            history: {
+              $each: newItems,
+            },
+          },
+        }
+      );
+      res.status(200).send(updatedChat);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error adding conversation!");
+    }
+  });
+  
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(401).send("Unauthenticated!");
+  });
+  
+  // PRODUCTION
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  });
+  
 
 app.listen(port, () => {
   connect();
